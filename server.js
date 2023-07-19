637b19e55e22310c3404f8a9761ec44de99777ae
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2023-07-19 14:43:21 -0700

    Setting: mongodb & mongoose setting

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const express = require('express');
+
+dotenv.config({ path: './config.env' });
+
 const app = express();
 const port = 8000;
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+});
 
 app.listen(port, () => {
   console.log(`server running, port: ${port}`);
