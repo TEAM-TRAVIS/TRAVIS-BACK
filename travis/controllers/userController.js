fcbfcf269ad30bac6a7274d588b05908fc3bf526
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-07-21 13:39:55 -0700

    feature: sign up

--- travis/controllers/userController.js
+++ travis/controllers/userController.js
@@ -1,16 +1,20 @@
 const UserModel = require('../models/userModel');
 
+exports.getSignup = (req, res) => {
+  res.render('signup'); // GET 요청 들어오면 signup 함수 실행하도록 설정
+};
+
 exports.signup = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { name, email, password } = req.body;
+    // 이미 사용 중인 이메일인지 확인
     const existingUser = await UserModel.findOne({ email });
 
     if (existingUser) {
       return res.status(400).json({ error: 'Email already exists' });
     }
 
-    const newUser = new UserModel({ email, password });
-    await newUser.save();
+    const user = await UserModel.create({ name, email, password });
 
     res.status(201).json({ message: 'User created successfully' });
   } catch (error) {
