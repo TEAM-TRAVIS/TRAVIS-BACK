9baf1077a798b4f95bdaa123b01e8fe7f43305f4
tmdtmdqorekf <tmdtmdqorekf@naver.com>
2023-07-22 20:41:47 -0700

    refactor: login error,  error code 302 -> 401

--- travis/routes/userRouter.js
+++ travis/routes/userRouter.js
@@ -14,13 +14,19 @@ router.post('/signup', userController.signup);
 router.get('/login', (req, res) => {
   res.render('login');
 });
-router.post(
-  '/login',
-  passport.authenticate('local', {
-    successRedirect: '/main', // 로그인 성공 시 메인 페이지로
-    failureRedirect: '/user/login', // 로그인 실패 시 로그인 페이지로
-  }),
-);
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user) => {
+    if (!user) {
+      return res.status(401).render('login', { message: 'Login failed' });
+    }
+    req.logIn(user, (err) => {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect('/main');
+    });
+  })(req, res, next); // 함수 내부에서 req, res, next 사용하기 위해 필요
+});
 
 // Logout
 router.get('/logout', userController.logout);
