61fe2e87699271ff46130191fba9154bd6e9eada
zkxmdkdltm <zmdk966@kookmin.ac.kr>
2023-07-30 22:04:24 -0700

    refactor: refactor async try catch block with catchAsync function

--- travis/controllers/userController.js
+++ travis/controllers/userController.js
@@ -1,26 +1,22 @@
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const UserModel = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
 // 회원가입
-exports.signup = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    // 이미 사용 중인 이메일인지 확인
-    const existingUser = await UserModel.findOne({ email });
-    console.log('회원가입 성공 테스트');
-    if (existingUser) {
-      return res.status(400).json({ error: 'Email already exists' });
-    }
-
-    const user = await UserModel.create({ name, email, password, joinDate: Date.now() });
-
-    res.status(201).json({ message: 'User created successfully', user });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Internal server error' });
+exports.signup = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  // 이미 사용 중인 이메일인지 확인
+  const existingUser = await UserModel.findOne({ email });
+  console.log('회원가입 성공 테스트');
+  if (existingUser) {
+    return res.status(400).json({ error: 'Email already exists' });
   }
-};
+
+  const user = await UserModel.create({ name, email, password, joinDate: Date.now() });
+
+  res.status(201).json({ message: 'User created successfully', user });
+});
 
 // 로그인
 exports.login = (req, res, next) => {
@@ -35,7 +31,7 @@ exports.login = (req, res, next) => {
 };
 
 // 로그아웃
-exports.logout = (req, res) => {
+exports.logout = (req, res, next) => {
   req.logout((err) => {
     if (err) {
       console.error('Error logging out:', err);
