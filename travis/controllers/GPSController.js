289c7e4becfcb37a818be2aa386acb0ca00817ae
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-08-03 14:28:24 -0700

    feature: Upload summary to mongoDB

--- travis/controllers/GPSController.js
+++ travis/controllers/GPSController.js
@@ -1,5 +1,6 @@
 const AWS = require('aws-sdk'); //AWS
 const dotenv = require('dotenv'); //.env 파일 읽는 라이브러리
+const GPSModel = require('../models/GPSModel');
 
 //.env 파일 load
 dotenv.config();
@@ -13,11 +14,11 @@ AWS.config.update({
 
 const s3 = new AWS.S3();
 
+
 //json 타입을 Buffer 타입으로 변환하는 함수
 const jsonToBuffer = async (jsonInput) => {
   const jsonString = JSON.stringify(jsonInput); // JSON 객체를 string로 변환
   const bufferFile = Buffer.from(jsonString); //string을 Buffer type으로 바꾸기
-
   return bufferFile;
 };
 
@@ -45,19 +46,43 @@ const uploadToS3 = async (uploadRoute, file) => {
 const getFileFromS3 = async (uploadRoute) => {
   const getObjectData = await s3.getObject(uploadRoute).promise();
   const file = getObjectData.Body.toString('utf-8'); //인코딩
-
   return file;
+};
+
+//몽고DB 업로드
+const saveToMongo = async (email, date, dist, time, svRt) => {
+  let GPSDB = await GPSModel.findOne({ id });
+
+  // 해당 id로 이전에 생성된 GPS 문서가 없는 경우
+  if (!GPSDB) {
+    GPSDB = new GPSModel({ email: email, records: [] });
+  }
+  // 새로운 레코드 추가
+  GPSDB.records.push({ date, dist, time, svRt });
+
+  try {
+    await GPSDB.save(); // 저장
+    console.log('GPS 데이터 업로드 성공!');
+  } catch (error) {
+    console.error('GPS 데이터 업로드 중 에러 발생:', error);
+  }
 };
 
 exports.saveGPS = async (req, res) => {
   try {
-    // 업로드 경로
+    const { email, date, dist, time, file } = req.body;
+
+    // S3 업로드 경로
     const uploadRoute = {
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: process.env.AWS_S3_FILE_PATH,
+      Key: `${email}/${time}`, // user1/2023080213440503
     };
 
-    const uploadedURL = await uploadToS3(uploadRoute, req.body); //업로드 후 업로드 경로를 변수에 저장.
+    //S3에 file 업로드
+    const uploadedURL = await uploadToS3(uploadRoute, file); //업로드 후 업로드 경로를 변수에 저장.
+
+    //몽고DB 업로드
+    saveToMongo(email, date, dist, time, uploadRoute.Key);
 
     const uploadedFile = await getFileFromS3(uploadRoute); //해당 S3 ROUTE로 파일 다시 GET.
 
