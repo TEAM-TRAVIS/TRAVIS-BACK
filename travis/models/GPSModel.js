14ddcc6bd7b5540d6cea13bfadd27c0df2464ad0
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-08-03 14:27:49 -0700

    feature : Modify MongoDB Schema

--- travis/models/GPSModel.js
+++ travis/models/GPSModel.js
@@ -3,18 +3,16 @@ const mongoose = require('mongoose');
 const GPSSchema = new mongoose.Schema({
   //userModel 참조
   email: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: 'UserModel',
-    required: [true, 'User id is required'],
+    required: [true, 'User email is required'],
   },
   records: [
     {
-      recordId: { type: String, required: [true, 'Record id is required'], unique: true },
-      date: { type: String, required: [true, 'Record date is required'] }, //측정일
-      text: { type: String, required: false }, //메모 (선택적)
-      duration: { type: String, required: [true, 'Total Duration time is required'] }, //총 이동시간 (stop 시에 측정됨)
-      distance: { type: String, required: [true, 'Total Distance is required'] }, //총 이동거리 (stop 시에 측정됨)
-      GPS: [{ data: Buffer }],
+      date: { type: Number, required: [true, 'Record date is required'] }, //측정일
+      dist: { type: Number, required: [true, 'Distance is required'] }, //총 이동거리
+      time: { type: Number, required: [true, 'Duration time is required'] }, //총 이동시간
+      svRt: { type: String, required: [true, 'AWS S3 save route is required'] }, //S3에 저장된 경로
     },
   ],
 });
