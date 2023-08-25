c538a15f4c1bd7d7901ab2c7184f7e5c638805a5
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-08-25 00:15:00 -0700

    refactor: imgController

--- travis/controllers/imgController.js
+++ travis/controllers/imgController.js
@@ -2,44 +2,37 @@ const catchAsync = require('../utils/catchAsync');
 const uploadToS3 = require('../utils/uploadToS3');
 const getFileFromS3 = require('../utils/getFileFromS3');
 
-exports.save = async (req, res) => {
-  try {
-    const { email } = req.body;
-    const image = req.file.buffer; // 이미지 데이터 추출
+// 프로필 이미지 저장
+exports.saveImage = catchAsync(async (req, res, next) => {
+  const { email } = req.body;
+  const image = req.file.buffer; // 이미지 데이터 추출
 
-    // S3 업로드 경로
-    const uploadRoute = {
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: `${email}/profile`, // user1@gmail.com/profile
-    };
+  // S3 업로드 경로
+  const uploadRoute = {
+    Bucket: process.env.AWS_S3_BUCKET_NAME,
+    Key: `${email}/profile`, // user1@gmail.com/profile
+  };
+  //S3에 file 업로드
+  await uploadToS3(uploadRoute, image, true); //업로드 후 업로드 경로를 변수에 저장.
 
-    //S3에 file 업로드
-    await uploadToS3(uploadRoute, image, true); //업로드 후 업로드 경로를 변수에 저장.
+  //response
+  return res.status(201).json({
+    message: 'profile이 s3에 성공적으로 저장됐습니다',
+    image: image,
+  });
+});
 
-    console.log('이미지 S3에 업로드 성공!');
-
-    //response
-    return res.status(201).json({
-      message: 'profile이 s3에 성공적으로 저장됐습니다',
-      image: image,
-    });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'imgController.js의 save()에서 오류가 발생했습니다.', ' error내용': error });
-  }
-};
-
+// 프로필 이미지 조회
 exports.getImage = catchAsync(async (req, res, next) => {
   const { email } = req.body; //url에 포함된 정보 추츨
   const uploadRoute = {
     Bucket: process.env.AWS_S3_BUCKET_NAME,
     Key: `${email}/profile`, // user1@mgmail.com/profile
   };
-  const gzipFile = await getFileFromS3(uploadRoute, true); //해당 S3 ROUTE로 파일 다시 GET.
+  const imageFile = await getFileFromS3(uploadRoute, true); //해당 S3 ROUTE로 파일 다시 GET.
   //response
   return res.status(201).json({
-    message: '프로필 사진 get 성공.',
-    image: gzipFile,
+    message: '프로필 사진 GET 성공.',
+    image: imageFile,
   });
 });
--- travis/routes/imgRouter.js
+++ travis/routes/imgRouter.js
@@ -8,18 +8,17 @@ const router = express.Router();
 const memStorage = multer.memoryStorage(); // 메모리 storage 선택
 const uploadToMem = multer({ storage: memStorage }); //메모리에 image 임시 저장
 
+// 프로필 이미지 저장
 //localhost:3000/img/save
 router.get('/save', (req, res) => {
-  console.log('여기까지 옴');
   res.render('save');
 });
 // POST 요청이 들어왔을 때, multer 미들웨어를 먼저 실행 -> request의 file 필드를 읽어서 서버 메모리에 임시 저장 -> imgController.save를 호출
-// 저장된 이미지는 req.file에
-router.post('/save', uploadToMem.single('file'), imgController.save);
+router.post('/save', uploadToMem.single('file'), imgController.saveImage);
 
+// 프로필 이미지 조회
 //localhost:3000/img/get
 router.get('/get', (req, res) => {
-  console.log('여기까지 옴');
   res.render('get');
 });
 router.post('/get', imgController.getImage);
