c6e48990dbe5584b7eb49168a45a9443c6ecdb66
hyeesw <hyeeun7904@kookmin.ac.kr>
2023-08-28 17:10:05 -0700

    feature: save GPS 'screenshot' to S3

--- travis/routes/GPSRouter.js
+++ travis/routes/GPSRouter.js
@@ -4,8 +4,16 @@ const getSummaryController = require('../controllers/getSummaryController');
 const getGPSController = require('../controllers/getGPSController');
 const saveGPSController = require('../controllers/saveGPSController');
 const accumulatedGPSController = require('../controllers/accumulatedGPSController');
+const multer = require('multer'); //이미지 request 처리
 
 const router = express.Router();
+
+// multer 설정
+const memStorage = multer.memoryStorage(); // 메모리 storage 선택
+const uploadToMem = multer({ storage: memStorage }); //메모리에 image 임시 저장
+
+// POST 요청이 들어왔을 때, multer 미들웨어를 먼저 실행 -> request의 SS 필드를 읽어서 서버 메모리에 임시 저장 -> saveGPSController.saveGPS를 호출
+router.post('/save', uploadToMem.single('file'), saveGPSController.saveGPS);
 
 // 마이페이지
 // router.get('/summary', (req, res) => {
@@ -33,8 +41,7 @@ router.get('/detail', (req, res) => {
 });
 router.route('/detail').post(getGPSController.getUserGPS).delete(getGPSController.deleteUserGPS);
 
-// [save] 눌렀을 때
-router.post('/save', saveGPSController.saveGPS);
+
 
 // x일 동안의 GPS 데이터 합침
 router.post('/accumulated', accumulatedGPSController.sendUserGpsSummary);
